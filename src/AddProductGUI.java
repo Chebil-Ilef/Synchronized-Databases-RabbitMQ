4984dd3f342c4f48c0c62d486840c4456ef81266
Lost Elf <ilef.chebil@insat.ucar.tn>
2024-03-04 23:48:32 +0100

    first commit

---
+++ src/AddProductGUI.java
@@ -0,0 +1,166 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.UUID;
+import java.time.LocalDate;
+import java.awt.Color;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.Font;
+
+
+public class AddProductGUI {
+    public final static String HO_QUEUE = "ho_queue";
+    public final static String HO_DBNAME = "HO";
+    public final String dbName;
+    public AddProductGUI(String dbName) {
+        this.dbName = dbName;
+        JFrame f = new JFrame(dbName+"database");
+        JTextField t1, t2, t3,  t5, t6, t7;
+        f.setSize(600, 1200);
+        JPanel p = new JPanel();
+        p.setBorder(new EmptyBorder(10, 10, 10, 10));
+        if (dbName=="HO"){p.setBackground(new Color(236, 226, 103));}
+        else {p.setBackground(new Color(236, 126, 103));}
+        p.setLayout(new GridLayout(9, 1, 15, 10));
+        f.add(p, BorderLayout.CENTER);
+        t1 = new JTextField("ProductName", 16);
+        JLabel productLabel = new JLabel("Product Name");
+        productLabel.setFont(new Font("Verdana", Font.BOLD, 16));
+        p.add(productLabel);
+        p.add(t1);
+
+        t2 = new JTextField("1",8);
+        JLabel qtyLabel = new JLabel("Quantity");
+        qtyLabel.setFont(new Font("Verdana", Font.BOLD, 16));
+        p.add(qtyLabel);
+        p.add(t2);
+
+        t3 = new JTextField("Sousse", 16);
+        JLabel regionLabel = new JLabel("Region");
+        regionLabel.setFont(new Font("Verdana", Font.BOLD, 16));
+        p.add(regionLabel);
+        p.add(t3);
+
+
+        t5 = new JTextField("1206",8);
+        JLabel costLabel = new JLabel("Cost");
+        costLabel.setFont(new Font("Verdana", Font.BOLD, 16));
+        p.add(costLabel);
+        p.add(t5);
+
+        t6 = new JTextField("6",8);
+        JLabel amtLabel = new JLabel("Amount");
+        amtLabel.setFont(new Font("Verdana", Font.BOLD, 16));
+        p.add(amtLabel);
+        p.add(t6);
+
+        t7 = new JTextField("10",8);
+        JLabel taxLabel = new JLabel("Tax");
+        taxLabel.setFont(new Font("Verdana", Font.BOLD, 16));
+        p.add(taxLabel);
+        p.add(t7);
+
+        JLabel label = new JLabel("");
+        p.add(label);
+
+        t5.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent ke) {
+                String value = t5.getText();
+                int l = value.length();
+                if (ke.getKeyChar() >= '0' && ke.getKeyChar() <= '9' || ke.getKeyChar() == '.') {
+                    t5.setEditable(true);
+                    label.setFont(new Font("Verdana", Font.BOLD, 16));
+                    label.setText("");
+                } else {
+                    t5.setEditable(false);
+                    label.setFont(new Font("Verdana", Font.BOLD, 16));
+                    label.setText("* Enter only numeric digits(0-9) or decimal point for cost field");
+                }
+            }
+        });
+
+        t6.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent ke) {
+                String value = t6.getText();
+                int l = value.length();
+                if (ke.getKeyChar() >= '0' && ke.getKeyChar() <= '9' || ke.getKeyChar() == '.') {
+                    t6.setEditable(true);
+                    label.setFont(new Font("Verdana", Font.BOLD, 16));
+                    label.setText("");
+                } else {
+                    t6.setEditable(false);
+                    label.setFont(new Font("Verdana", Font.BOLD, 16));
+                    label.setText("* Enter only numeric digits(0-9) or decimal point for amount field");
+                }
+            }
+        });
+
+        t7.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent ke) {
+                String value = t7.getText();
+                int l = value.length();
+                if (ke.getKeyChar() >= '0' && ke.getKeyChar() <= '9' || ke.getKeyChar() == '.') {
+                    t7.setEditable(true);
+                    label.setFont(new Font("Verdana", Font.BOLD, 16));
+                    label.setText("");
+                } else {
+                    t7.setEditable(false);
+                    label.setFont(new Font("Verdana", Font.BOLD, 16));
+                    label.setText("* Enter only numeric digits(0-9) or decimal point for tax field");
+                }
+            }
+        });
+
+
+        JButton addBtn=new JButton("Add product");
+        addBtn.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                String product = t1.getText();
+                String region = t3.getText();
+                int qty;
+                try {
+                    qty = Integer.parseInt(t2.getText());
+                } catch (NumberFormatException ex) {
+                    qty = 0; // or some other default value
+                }
+
+                float cost = Float.parseFloat(t5.getText());
+                float amt = Float.parseFloat(t6.getText());
+                float tax = Float.parseFloat(t7.getText());
+                float total = (qty *  cost) + amt + (tax * (qty * cost));
+                Product p = new Product( product, qty, cost, amt, tax, total, region);
+                //  If it is HO then it will only create a product in the HO database
+                // else it will create in BO and send a message the rabbitmq with flag = "ADD" and the product to add
+                ManageData.sendToDB(p,dbName);
+                // To fix  it
+                if (dbName != HO_DBNAME){
+                    ManageData.sendToHO(p);}
+                JFrame f = new JFrame("retrieved data");
+                JPanel p0 = new JPanel();
+                String column[] = {"id","product","qty","cost","amt","tax","total","region"};
+                String[][] products = ManageData.getAllProducts(dbName);
+                label.setText("* db updated with product "+ product);
+                JOptionPane.showMessageDialog(null, "product "+ p.product +" added successfully!");
+            }
+        });
+        JButton showBtn = new JButton("Show products");
+        showBtn.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                new VisualiseData(dbName);
+            }
+        });
+
+        p.add(addBtn); p.add(showBtn);
+
+        f.add(p);
+        f.setSize(450,550);
+        f.setVisible(true);
+    }
+}
+
---
+++ src/ManageData.java
@@ -0,0 +1,207 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.time.LocalDate;
+
+
+public class ManageData {
+    public final static String HO_QUEUE = "ho_queue";
+    public final static String HO_DBNAME = "HO";
+
+    // Creates a database with the specified name if it doesn't exist
+    public static void createDb(String dbName){
+        try{
+            // Establish connction to MySQL server
+            java.sql.Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3304/" , "root" , "ilef");
+            Statement stmt=con.createStatement();
+            // Execute SQL query to create the database if not exists
+            Boolean result = stmt.execute("CREATE DATABASE if not exists "+dbName);
+            System.out.println("success");
+            // Close the connection
+            con.close();
+        }catch( Exception e){
+            // Print error message if database creation fails
+            System.out.println("error in creating data base "+ dbName);
+            System.out.println(e);
+        }
+    }
+    // Creates a product table in the specified database if it doesn't exist
+    public static void createProductTable(String dbName){
+        try{
+            // Establish connection to the specified database
+            java.sql.Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3304/"+dbName,"root","ilef");
+            // Create a statement for executing SQL queries
+            Statement stmt=con.createStatement();
+            // SQL query to create 'product' table with required columns
+            // Add the primary Key to id to solve duplication problem
+            String sql = "CREATE TABLE IF NOT EXISTS product (" +
+                    "id VARCHAR(100) PRIMARY KEY , " +
+                    "product VARCHAR(50)," +
+                    "qty INT," +
+                    "cost FLOAT," +
+                    "amt FLOAT," +
+                    "tax FLOAT," +
+                    "total FLOAT," +
+                    "region VARCHAR(50)" +
+                    ")";
+            // Execute the SQL query
+            stmt.executeUpdate(sql);
+            con.close();
+        }catch(Exception e){
+            System.out.println("error in creating the product table in database "+ dbName);
+            System.out.println(e);
+        }
+    }
+    // Sends a Product object to be stored in the specified database (insert or replace a product in the database)
+    public static void sendToDB(Product p, String dbName){
+        try{
+            java.sql.Connection con= DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3304/"+dbName,"root","ilef");
+
+            String sql = "replace into product (id, product, qty, cost, amt, tax, total, region) values (?, ?, ?, ?, ?, ?, ?, ?);";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, p.id);
+            stmt.setString(2, p.product);
+            stmt.setInt(3, p.qty);
+            stmt.setFloat(4, p.cost);
+            stmt.setFloat(5, p.amt);
+            stmt.setFloat(6, p.tax);
+            stmt.setFloat(7, p.total);
+            stmt.setString(8, p.region);
+            stmt.executeUpdate();
+            con.close();
+        }catch(Exception e){
+            System.out.println("failed to send the product: " + p.product );
+            System.out.println(e);
+        }
+    }
+    // Retrieves all products from the specified database and returns them
+    public static String[][] getAllProducts(String dbName){
+        String[][] products = new String[100][9];
+        int i=0;
+        try {
+            java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3304/" + dbName, "root", "ilef");
+            Statement stmt = con.createStatement();
+            String sql = "SELECT * FROM product;";
+            ResultSet rs =  stmt.executeQuery(sql);
+            while (rs.next()) {
+                products[i][0] = rs.getString(1);
+                products[i][1] = rs.getString(2);
+                products[i][2] = Integer.toString(rs.getInt(3));
+                products[i][3] = Float.toString(rs.getFloat(4));
+                products[i][4] = Float.toString(rs.getFloat(5));
+                products[i][5] = Float.toString(rs.getFloat(6));
+                products[i][6] = Float.toString(rs.getFloat(7));
+                products[i][7] = rs.getString(8);
+                i++;
+            }
+
+
+        }catch (Exception e){
+            System.out.println("Error in retrieving data from "+ dbName );
+        }
+        String[][] products1 = new String[i][9];
+        for(int j=0 ; j<i; j++){
+            products1[j][0] = products[j][0];
+            products1[j][1] = products[j][1];
+            products1[j][2] = products[j][2];
+            products1[j][3] = products[j][3];
+            products1[j][4] = products[j][4];
+            products1[j][5] = products[j][5];
+            products1[j][6] = products[j][6];
+            products1[j][7] = products[j][7];
+        }
+        return products1;
+    }
+    // Sends a product to the Head Office (HO) via RabbitMQ for processing
+    public static void sendToHO(Product p) {
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            // Establish a connection to RabbitMQ
+            Connection connection = factory.newConnection();
+            // Create a channel for communication
+            Channel channel = connection.createChannel();
+            // Declare the queue for HO
+            channel.queueDeclare(HO_QUEUE, false, false, false, null);
+            // Construct the message to be sent to HO (using the "ADD" flag to indicate addition of a product)
+            String msg = "ADD"+p.toString();
+            // Publish the message to the HO queue
+            channel.basicPublish("", HO_QUEUE, null, msg.getBytes());
+        }catch (Exception e){
+            System.out.println("error sending data to HO ");
+        }
+    }
+
+    public static void sendQueryToHO(String sql) {
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+            channel.queueDeclare(HO_QUEUE, false, false, false, null);
+            String msg = sql.toString();
+            channel.basicPublish("", HO_QUEUE, null, msg.getBytes());
+        }catch (Exception e){
+            System.out.println("error sending data to HO ");
+        }
+    }
+    // Sends old data from the specified database to HO
+    public static void sendOldDataToHO(String dbName)  {
+        try {
+            java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3304/" + dbName, "root", "ilef");
+            Statement stmt = con.createStatement();
+            String sql = "SELECT * FROM product;";
+            ResultSet product =  stmt.executeQuery(sql);
+            while (product.next()){
+                String id = product.getString(1);
+                String productname = product.getString(2);
+                float qty = product.getInt(3);
+                float cost = product.getFloat(4);
+                float amt = product.getFloat(5);
+                float tax = product.getFloat(6);
+                float total = product.getFloat( 7);
+                String region = product.getString(8);
+                Product p = new Product(id,productname, (int) qty, cost, amt, tax, total, region);
+                sendToHO(p);
+            }
+
+
+        }catch (Exception e){
+        }
+    }
+
+    public static void execQuery(String sql,String dbName){
+        try{
+            java.sql.Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3304/"+dbName,"root","ilef");
+            Statement stmt=con.createStatement();
+            stmt.executeUpdate(sql);
+        }catch(Exception e){
+            System.out.println("error in executing  query : "+ sql);
+        }
+    }
+
+    public static  void removeFromBD(String id, String dbName){
+        String sql = "DELETE FROM product WHERE id = '"+id+"';";
+        execQuery(sql,dbName);
+    }
+
+    public static void removeFromHO(String id){
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+            channel.queueDeclare(HO_QUEUE, false, false, false, null);
+            channel.basicPublish("", HO_QUEUE, null, ("DEL"+id).getBytes());
+        }catch (Exception e){
+            System.out.println("error sending data to HO ");
+        }
+    }
+
+
+}
